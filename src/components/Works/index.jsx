2d5eda3158d7c8da2959ad571df28676d5f8e215
Rxses <35810818+rxses-x@users.noreply.github.com>
2025-03-10 17:49:46 +0100

    feat(Works): add margin bottom

--- src/components/Works/index.jsx
+++ src/components/Works/index.jsx
@@ -6,7 +6,7 @@ const Works = forwardRef(({ data }, ref) => {
         <section ref={ref} className="mt-10 laptop:mt-30">
             <h2 className="text-2xl text-bold">Works.</h2>
 
-            <div className="mt-5 laptop:mt-10 grid grid-cols-1 tablet:grid-cols-2 gap-4">
+            <div className="mt-5 mb-10 laptop:mt-10 grid grid-cols-1 tablet:grid-cols-2 gap-4">
                 {data.projects.map((project) => (
                     <WorkCards
                         key={project.id}
