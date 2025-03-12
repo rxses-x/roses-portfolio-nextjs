b385d8986ca2bf29cbafc1ef6f873eb716c295ab
Rxses <35810818+rxses-x@users.noreply.github.com>
2025-03-12 18:55:00 +0100

    Update build for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: "/roses-portfolio-nextjs",
   images: {
     domains: ['images.unsplash.com'],
     unoptimized: true,
