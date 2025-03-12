2c41a999c214c0f44d0ab4ea803d603177b519d7
Rxses <35810818+rxses-x@users.noreply.github.com>
2025-03-12 19:00:28 +0100

    Re-add basePath for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: "/roses-portfolio-nextjs",
   images: {
     domains: ['images.unsplash.com'],
     unoptimized: true,
