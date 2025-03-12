1dba21f67046595b7c3eb55228ec08d706a084c1
Rxses <35810818+rxses-x@users.noreply.github.com>
2025-03-12 18:45:38 +0100

    fix path

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: "roses-portfolio-nextjs/",
   images: {
     domains: ['images.unsplash.com'],
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/roses-portfolio-nextjs' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/roses-portfolio-nextjs/' : '',
-  trailingSlash: true,
 };
 
 export default nextConfig;
