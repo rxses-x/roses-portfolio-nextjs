d4621f4742666222d75f683e878ce87c281db493
Rxses <roses.x@outlook.com>
2025-03-13 12:55:33 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  assetPrefix: "/roses-portfolio-nextjs/",
-  basePath: "/roses-portfolio-nextjs",
   images: {
     domains: ['images.unsplash.com'],
     unoptimized: true,
