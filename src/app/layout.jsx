6d9e9ee3947a260b9e22b3d59ecff308e70de48b
Rxses <35810818+rxses-x@users.noreply.github.com>
2025-03-14 14:07:15 +0100

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -18,7 +18,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider>{children}</ThemeProvider>
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
